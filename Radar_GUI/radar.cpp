21b182359c35706fc057f528659fdb47c8423e4e
SIMAR MANN SINGH <simar1992@live.in>
2018-06-06 17:40:42 +0200

    checkpoint radar PPI display... Circle not running.

--- Radar_GUI/radar.cpp
+++ Radar_GUI/radar.cpp
@@ -10,6 +10,9 @@
 #include <stdlib.h>				
 #include <stdio.h>
 
+float centerX = 320.0;
+float centerY = 240.0;
+float PI_180 = M_PI / 180.0f;
 
 int main(int argc, char *argv[])
 {
@@ -92,14 +95,121 @@ int main(int argc, char *argv[])
 		}
 		glEnd();
 
-		/*glBegin(GL_POLYGON);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		glLineWidth(2.5);
+		glBegin(GL_LINES);
 		for (int i = 0; i < 360; i++) {
 			float degInRad = i * DEG2RAD;
-			glVertex2f(cos(degInRad)*(0.01), radius));
-		}															//this need to be updated with code for drawing a line connecting center of radar to last circle of radar
+			glVertex2f(0.01, 0.01);
+			//glVertex2f(0.72, 0.72);
+			glVertex2f(cos(degInRad)*(radius), sin(degInRad)*(radius));
+		}
+		
+		//glVertex2f(cos(degInRad)*(0.01), sin(degInRad)*(0.01));
+			
 		glEnd();
 
-		*/
+
+
+
+
+
+
+
+
+
+
+		glPopMatrix();
+
+		//Display 45 deg crosshair
+
+		glDisable(GL_TEXTURE_2D);
+		glEnable(GL_LINE_SMOOTH);
+
+		glLineWidth(1.5);
+		glPushMatrix();
+		glColor3f(0.712, 0.767, 0.431);
+		glBegin(GL_LINES);
+		for (float angle = 0.0; angle <= 360.0; angle += 45.0)
+		{
+			float n_cos = cos(angle * PI_180);
+			float n_sin = sin(angle * PI_180);
+			glVertex2f(centerX + n_cos * radius / 8, centerY + n_sin * radius / 8);
+			glVertex2f(centerX + n_cos * radius, centerY + n_sin * radius);
+
+		}
+
+		//Display Degrees
+
+		for (int angle = 0; angle <= 360; angle += 1)
+		{
+			float n_cos = cos(angle * PI_180);
+			float n_sin = sin(angle * PI_180);
+			if (angle % 5 == 0)
+			{
+				glLineWidth(2.5);
+				glVertex2f(centerX + n_cos * (radius + (rad_64)), centerY + n_sin * (radius + (rad_64)));
+				glVertex2f(centerX + n_cos * (radius + (radius / 20)), centerY + n_sin * (radius + (radius / 20)));
+			}
+			else
+			{
+				glLineWidth(1.5);
+				glVertex2f(centerX + n_cos * (radius + (rad_64)), centerY + n_sin * (radius + (rad_64)));
+				glVertex2f(centerX + n_cos * (radius + (radius / 30)), centerY + n_sin * (radius + (radius / 30)));
+			}
+			if ((angle + 22) % 45 == 0)
+			{
+				glVertex2f(centerX + n_cos * (rad_3 - rad_64), centerY + n_sin * (rad_3 - rad_64));
+				glVertex2f(centerX + n_cos * (rad_3 + rad_64), centerY + n_sin * (rad_3 + rad_64));
+
+				glVertex2f(centerX + n_cos * (2 * rad_3 - rad_64), centerY + n_sin * (2 * rad_3 - rad_64));
+				glVertex2f(centerX + n_cos * (2 * rad_3 + rad_64), centerY + n_sin * (2 * rad_3 + rad_64));
+			}
+		}
+		glEnd();
+		glLineWidth(2.5);
+		glBegin(GL_LINE_LOOP);
+
+		//Display outer circle
+
+		for (int angle = 0; angle <= 360; angle += 1)
+		{
+			glVertex2f(centerX + cos(angle * PI_180) * (radius + (rad_64)), centerY + sin(angle * PI_180) * (radius + (rad_64)));
+		}
+
+		glEnd();
+
+		glLineWidth(2.0);
+		glBegin(GL_LINE_LOOP);
+
+		//Display inner circle
+
+		for (int angle = 0; angle <= 360; angle += 1)
+		{
+			glVertex2f(centerX + cos(angle * PI_180) * (radius + 1), centerY + sin(angle * PI_180) * (radius + 1));
+		}
+
+		glEnd();
+
+		glLineWidth(2.0);
+		glBegin(GL_LINE_STRIP);
+
+		//Display center circles
+
+		for (int angle = 0; angle <= 360; angle += 1)
+		{
+			glVertex2f(centerX + cos(angle * PI_180) * (rad_3), centerY + sin(angle * PI_180) * (rad_3));
+		}
+
+		for (int angle = 0; angle <= 360; angle += 1)
+		{
+			glVertex2f(centerX + cos(angle * PI_180) * (2 * rad_3), centerY + sin(angle * PI_180) * (2 * rad_3));
+		}
+
+		glEnd();
+
+
+
+
 
 
 		/* Swap front and back buffers */
