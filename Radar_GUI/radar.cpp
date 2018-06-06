08aef205f776ffd473257b3a46e711e099b90300
SIMAR MANN SINGH <simar1992@live.in>
2018-06-06 17:09:20 +0200

    Basic Radar PPI display in progress

--- Radar_GUI/radar.cpp
+++ Radar_GUI/radar.cpp
@@ -25,16 +25,19 @@ int main(int argc, char *argv[])
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+
+	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 
-	//constants used in the program
+	/*constants used in the program*/
 	const float DEG2RAD = 3.14159 / 180;
-	float radius = 0.25;
+	float radius = 0.96;
 	float r = 0.31;
 	float g = 0.2;
 	float b = 0.75;
 
+	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
 		//setup view
 		float ratio;
@@ -44,22 +47,62 @@ int main(int argc, char *argv[])
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		/*
 		//Color 
 		r = fmod(r + 0.01, 1);
 		g = fmod(g + 0.02, 1);
 		b = fmod(b + 0.03, 1);
-		
+		*/
+
 		//Drawing
-		glColor3f(r, g, b);
-		glBegin(GL_POLYGON);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		//glColor3f(r, g, b);
+		glBegin(GL_LINE_STRIP);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
 		for (int i = 0; i < 360; i++) {
 			float degInRad = i * DEG2RAD;
-			glVertex2f(cos(degInRad)*radius, sin(degInRad)*radius);
-
+			glVertex2f(cos(degInRad)*radius, sin(degInRad)*radius);		
 		}
 		glEnd();
 
-		//swap buffer and check for events 
+
+		glBegin(GL_LINE_STRIP);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*(radius / 2), sin(degInRad)*(radius / 2));
+		}
+		glEnd();
+
+		glBegin(GL_LINE_STRIP);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*(0.72), sin(degInRad)*(0.72));
+		}
+		glEnd(); 
+		
+		glBegin(GL_LINE_STRIP);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*(radius / 4), sin(degInRad)*(radius / 4));
+		}
+		glEnd();
+
+		glBegin(GL_POLYGON);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*(0.01), sin(degInRad)*(0.01));
+		}
+		glEnd();
+
+		/*glBegin(GL_POLYGON);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*(0.01), radius));
+		}															//this need to be updated with code for drawing a line connecting center of radar to last circle of radar
+		glEnd();
+
+		*/
+
+
+		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
