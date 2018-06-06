913715cf9d51fd463ac0c9bca0cf596fb493a377
SIMAR MANN SINGH <simar1992@live.in>
2018-06-06 14:55:56 +0200

    basic Open GL program to setup the environment and set the code up and running in no time. A BASIC TUTORIAL for beginners.

---
+++ Radar_GUI/radar.cpp
@@ -0,0 +1,71 @@
+/*
+
+   Author			: Simar Mann Singh
+   Reference		: Intro to Computer Science (Link : https://www.youtube.com/watch?v=kwxCP_LLZJ4&t=328s)
+
+*/
+
+#include<GLFW/glfw3.h>
+#include"linmath.h"
+#include <stdlib.h>				
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);										//defines the veriosn of the openGL used 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	GLFWwindow* window = glfwCreateWindow(480, 480, "OpenGL using GLFW", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	//constants used in the program
+	const float DEG2RAD = 3.14159 / 180;
+	float radius = 0.25;
+	float r = 0.31;
+	float g = 0.2;
+	float b = 0.75;
+
+	while (!glfwWindowShouldClose(window)) {
+		//setup view
+		float ratio;
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Color 
+		r = fmod(r + 0.01, 1);
+		g = fmod(g + 0.02, 1);
+		b = fmod(b + 0.03, 1);
+		
+		//Drawing
+		glColor3f(r, g, b);
+		glBegin(GL_POLYGON);											// GL_LINE_STRIP or GL_POLYGON defines whether the shape drawn would be a line shape or a filled polygon
+		for (int i = 0; i < 360; i++) {
+			float degInRad = i * DEG2RAD;
+			glVertex2f(cos(degInRad)*radius, sin(degInRad)*radius);
+
+		}
+		glEnd();
+
+		//swap buffer and check for events 
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();													// glfwTerminate() should always be used before exiting the code to release the resources the program is using
+	exit(EXIT_SUCCESS);
+
+
+}
